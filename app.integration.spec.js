15abe0d5b348ed533a62cd837e5154f91f21c81a
chaoyuquenet <chaoyu1979@gmail.com>
2019-03-26 17:02:07 +0100

    add test

--- app.integration.spec.js
+++ app.integration.spec.js
@@ -1,6 +1,9 @@
 const request = require('supertest');
 
+// const dataInterface = require('./data-interface');
 const app = require('./app');
+const dataInterface = require('./data-interface');
+const { createMessage } = require('./data-interface');
 const agent = request.agent(app);
 
 describe('app', () => {
@@ -15,26 +18,40 @@ describe('app', () => {
       describe('with non-empty content', () => {
         describe('with JavaScript code in personalWebsiteURL', () => {
           it('responds with error', async done => {
-            await agent
+            const response = await agent
               .post('/messages')
               .send(
                 "content=test&personalWebsiteURL=javascript:alert('Hacked!');"
-              )
-              .expect('Content-Type', 'application/json; charset=utf-8')
-              .expect(400);
+              );
+            expect(response.status).toEqual(400);
             done();
+          });
+
+          it('does not call createMessage function', () => {
+            dataInterface.createMessage = jest.fn();
+            expect(dataInterface.createMessage).toHaveBeenCalledTimes(0);
           });
         });
 
         describe('with HTTP URL in personalWebsiteURL', () => {
-          it('responds with success', async done => {
-            await agent
+          it('responds with success and create message', async done => {
+            const response = await agent
               .post('/messages')
               .send(
                 'content=test&personalWebsiteURL=https://fr.wikipedia.org/wiki/Cross-site_scripting'
-              )
-              .expect('Content-Type', 'application/json; charset=utf-8')
-              .expect(201);
+              );
+            expect(response.status).toEqual(201);
+            done();
+          });
+
+          it('call createMessage function', async done => {
+            dataInterface.createMessage = jest.fn();
+            const response = await agent
+              .post('/messages')
+              .send(
+                'content=test&personalWebsiteURL=https://fr.wikipedia.org/wiki/Cross-site_scripting'
+              );
+            expect(dataInterface.createMessage).toHaveBeenCalledTimes(1);
             done();
           });
         });
